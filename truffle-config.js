0148f65416b0b3dd1585ab9929d6425718e99555
Kristjan Tiido <kristjan.tiido@gmail.com>
2021-11-14 00:28:44 +0200

    """ Test commit """

--- truffle-config.js
+++ truffle-config.js
@@ -1,22 +1,20 @@
-require('babel-register');
-require('babel-polyfill');
-
 module.exports = {
   networks: {
     development: {
-      host: "127.0.0.1",
-      port: 7545,
-      network_id: "*" // Match any network id
-    },
+      host: "localhost",
+      port: 8545,
+      network_id: "*", // Match any network id
+      gas: 5000000
+    }
   },
-  contracts_directory: './src/contracts/',
-  contracts_build_directory: './src/abis/',
   compilers: {
     solc: {
-      optimizer: {
-        enabled: true,
-        runs: 200
+      settings: {
+        optimizer: {
+          enabled: true, // Default: false
+          runs: 200      // Default: 200
+        },
       }
     }
   }
-}
+};
